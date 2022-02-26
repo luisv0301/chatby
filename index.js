e3c4d31fd56f6aecf19061d21e837cac30fe0ed0
Luis Vasquez <luisvbdr0301@gmail.com>
2022-02-26 16:24:54 -0430

    fix: change cta buttons

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const btns = document.getElementsByClassName("social-btn");
 const facebookBtn = btns[2];
-const wave = document.getElementById("wave");
+const waves = document.getElementsByClassName("wave");
+const facebookWave = waves[2];
 const waveBgColors = ["25D366", "8E0FD5FA", "0099FF"];
+const images = [
+  "images/mockup-for-whatsApp.png",
+  "images/mockup-for-instagram.png",
+  "images/mockup-for-messenger.png",
+];
+const mockUp = document.getElementById("phone");
 let btnsCounter = 0;
 let timerCounter = 2;
 
@@ -9,30 +16,36 @@ facebookBtn.classList.remove("isGray");
 
 const timer = setInterval(() => {
   btns[timerCounter].classList.add("isGray");
+  waves[timerCounter].classList.add("hideWave");
   timerCounter++;
   if (timerCounter > 2) {
     timerCounter = 0;
   }
   btns[timerCounter].classList.remove("isGray");
-  wave.style.setProperty("--bg-color", "#" + waveBgColors[timerCounter]);
+  mockUp.setAttribute("src", images[timerCounter]);
+  waves[timerCounter].classList.remove("hideWave");
 }, 5000);
 
 Array.from(btns).forEach((btn, index) => {
   btn.addEventListener("click", () => {
     if (timer) {
       btns[timerCounter].classList.add("isGray");
+      waves[timerCounter].classList.add("hideWave");
       clearInterval(timer);
     }
 
     btns[btnsCounter].classList.add("isGray");
-    btn.classList.remove("isGray");
-    wave.style.setProperty("--bg-color", "#" + waveBgColors[index]);
+    waves[btnsCounter].classList.add("hideWave");
     btnsCounter = index;
+    btn.classList.remove("isGray");
+    mockUp.setAttribute("src", images[btnsCounter]);
+    waves[btnsCounter].classList.remove("hideWave");
   });
 });
 
 if (window.matchMedia("(max-width: 520px)").matches) {
   [...btns].forEach((btn) => btn.classList.remove("isGray"));
+  const clasess = ["whatsapp", "instagram", "messenger"];
   const arrows = document.querySelectorAll(".button-arrow");
 
   let count = 2;
@@ -51,10 +64,7 @@ if (window.matchMedia("(max-width: 520px)").matches) {
         count = 2;
       }
       btns[count].classList.remove("hide");
-      document.documentElement.style.setProperty(
-        "--bg-color-btn",
-        "#" + waveBgColors[count]
-      );
+      mockUp.setAttribute("src", images[count]);
     });
   });
 }
