76a65a6a3c5e3fce92882839667f6b84ffeb5b87
Luis Vasquez <luisvbdr0301@gmail.com>
2022-02-27 11:00:48 -0430

    fix: optimise images

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const images = [
   "images/mockup-for-instagram.png",
   "images/mockup-for-messenger.png",
 ];
-
 let preloaded = [];
-
 const mockUp = document.getElementById("phone");
 let btnsCounter = 0;
 let timerCounter = 2;
@@ -36,64 +34,6 @@ const timer = setInterval(() => {
   waves[timerCounter].classList.remove("hideWave");
 }, 5000);
 
-/*
-const timer = setInterval(() => {
-  btns[timerCounter].classList.add("isGray");
-  waves[timerCounter].classList.add("hideWave");
-  timerCounter++;
-  if (timerCounter > 2) {
-    timerCounter = 0;
-  }
-  btns[timerCounter].classList.remove("isGray");
-  mockUp.setAttribute("src", images[timerCounter]);
-  waves[timerCounter].classList.remove("hideWave");
-}, 5000);
-
-
-btnsArray.forEach((btn, index) => {
-  if (!isSmallScreen) {
-    btn.addEventListener("click", () => {
-      if (timer) {
-        btns[timerCounter].classList.add("isGray");
-        waves[timerCounter].classList.add("hideWave");
-        clearInterval(timer);
-      }
-      btns[btnsCounter].classList.add("isGray");
-      waves[btnsCounter].classList.add("hideWave");
-      btnsCounter = index;
-      btn.classList.remove("isGray");
-      mockUp.setAttribute("src", images[btnsCounter]);
-      waves[btnsCounter].classList.remove("hideWave");
-    });
-  }
-});
-
-
-if (isSmallScreen) {
-  [...btns].forEach((btn) => btn.classList.remove("isGray"));
-  const arrows = document.querySelectorAll(".button-arrow");
-
-  let count = 2;
-  if (timer) {
-    clearInterval(timer);
-  }
-
-  arrows.forEach((arrowBtn) => {
-    arrowBtn.addEventListener("click", () => {
-      btns[count].classList.add("hide");
-      arrowBtn.id == "less" ? count-- : count++;
-      if (count > 2) {
-        count = 0;
-      }
-      if (count < 0) {
-        count = 2;
-      }
-      btns[count].classList.remove("hide");
-      mockUp.setAttribute("src", images[count]);
-    });
-  });
-}
-*/
 btnsArray.forEach((btn, index) => {
   if (!isSmallScreen) {
     btn.addEventListener("click", () => {
@@ -186,7 +126,6 @@ function scrollFunction() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("cargando dom listo");
   images.forEach((image) => {
     let im = new Image();
     im.src = image;
